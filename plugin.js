2fbe9d069eff6d1598f9d7077fc30a688db0a779
Axel Hecht <axel@pike.org>
2015-04-26 12:39:07 +0200

    core plugin structure

---
+++ plugin.js
@@ -0,0 +1,46 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ 
+define(function(require, exports, module) {
+    "use strict";
+    main.consumes = ["Plugin", "language"];
+    main.provides = ["moz.aisle"];
+    return main;
+
+    function main(options, imports, register) {
+        var Plugin = imports.Plugin;
+        
+        /***** Initialization *****/
+        
+        var plugin = new Plugin("Ajax.org", main.consumes);
+        var emit = plugin.getEmitter();
+
+        function load() {
+            console.log('aisle loaded');
+        }
+        
+        /***** Methods *****/
+        
+        
+        
+        /***** Lifecycle *****/
+        
+        plugin.on("load", function() {
+            load();
+        });
+        plugin.on("unload", function() {
+        
+        });
+        
+        /***** Register and define API *****/
+        
+        plugin.freezePublicAPI({
+            
+        });
+        
+        register(null, {
+            "moz.aisle": plugin
+        });
+    }
+});
